5d83430e4e1a0026b2a80b635dd9ed878b338530
Emir Kotan <emirkotan@gmail.com>
2017-10-03 12:52:19 +0100

    Start over.

---
+++ include/def.h
@@ -0,0 +1,21 @@
+#ifndef DEF_H
+#define DEF_H
+
+#include <stddef.h>
+
+#define DOWL(x) \
+	do { x } while (0)
+
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER) \
+	((size_t) &((TYPE *)0)->MEMBER)
+#endif
+
+#define container_of(PTR, TYPE, MEMBER) \
+	((TYPE *)((char *)(1 ? PTR : &((TYPE *)0)->MEMBER)\
+	- offsetof(TYPE, MEMBER)))
+
+typedef unsigned char byte;
+typedef size_t uint;
+
+#endif
